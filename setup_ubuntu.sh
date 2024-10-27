1405b57094d9a61ecf1ba4d18787e3a04e09ebd2
Stepan Kruglov <kruglovstepan367@gmail.com>
2024-10-27 21:22:24 +0300

    Pass yes option in rust-init scrips

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -127,7 +127,7 @@ install_rust() {
     printf "%s\n" "Rust is already installed"
   else
     printf "%s\n" "Rust is not found. Install it"
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -yq
   fi
   rustup update
 
