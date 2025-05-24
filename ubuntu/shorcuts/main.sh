1a446ba73b6863efde32a355494ea8c69d94c07f
Stepan Kruglov <kruglovstepan367@gmail.com>
2025-05-24 18:58:36 +0300

    fix

--- ubuntu/shorcuts/main.sh
+++ ubuntu/shorcuts/main.sh
@@ -2,13 +2,12 @@
 
 # allow start xream witout sudo
 # reboot reboot PC
-user=$(whoami)
-sudo gpasswd -a "$user" input
-echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/input.rules
+# user=$(whoami)
+# sudo gpasswd -a "$user" input
+# echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/input.rules
 
 # setup systemd service
-cp xremap-macos-shortcuts-mimic.service "$XDG_CONFIG_HOME/.systemd/user/"
+cp xremap.service "$XDG_CONFIG_HOME/systemd/user/"
 systemctl --user daemon-reload
-systemctl --user enable xremap-macos-shortcuts-mimic.service
-systemctl --user start xremap-macos-shortcuts-mimic.service
-
+systemctl --user enable xremap.service
+systemctl --user start xremap.service
