2db8526fdab2affd5589971c261fc93045bf0a4d
Stepan Kruglov <kruglovstepan367@gmail.com>
2025-07-04 08:35:44 +0300

    after ubuntu run

--- ubuntu/test.sh
+++ ubuntu/test.sh
@@ -7,7 +7,8 @@ tempdir() {
   local tmpdir
   tempprefix=$(basename "$0")
   # https://unix.stackexchange.com/questions/30091/fix-or-alternative-for-mktemp-in-os-x
-  tmpdir=$(mktemp -d -t "$tempprefix") || {
+  #tmpdir=$(mktemp -d -t "$tempprefix" 2>/dev/null) || {
+  tmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t "$tempprefix") || {
     #log_warn "Create tmp directory is failure."
     echo "Create tmp directory is failure."
     return 1
@@ -19,10 +20,15 @@ WORKSTATION_TMP_DIR="$(tempdir || exit 1)"
 trap '{ rm -rf -- "$WORKSTATION_TMP_DIR"; }' EXIT
 
 install_nerd_fonts() {
-  log_info "Install Nerd Fonts."
+  #log_info "Install Nerd Fonts."
+  echo "Install Nerd Fonts."
   local temp_dir="$WORKSTATION_TMP_DIR\nerd-fonts"
   git clone --depth=1 -q --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git "$temp_dir"
-  git -C "$temp_dir" sparse-checkout add patched-fonts/JetBrainsMono
-  bash "${temp_dir}/install.sh" JetBrainsMono
-  rm -rf "$temp_dir"
+  echo $temp_dir 
+  ls $temp_dir 
+  # git -C "$temp_dir" sparse-checkout add patched-fonts/JetBrainsMono
+  # bash "${temp_dir}/install.sh" JetBrainsMono
+  # rm -rf "$temp_dir"
 }
+
+install_nerd_fonts
