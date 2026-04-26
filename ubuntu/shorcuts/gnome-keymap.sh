5951bb35d90ba3cea69564437014167a12a18103
Stepan Kruglov <24195348+stepankv@users.noreply.github.com>
2026-04-26 08:38:44 +0300

    fix(shortcuts): Remove repeated key bindings

--- ubuntu/shorcuts/gnome-keymap.sh
+++ ubuntu/shorcuts/gnome-keymap.sh
@@ -5,12 +5,12 @@ set -u
 
 declare -A keymap=(
   ["/org/gnome/desktop/wm/keybindings/activate-window-menu"]="['']"           # Disable window menu
-  ["/org/gnome/mutter/edge-tiling"]="false"                                   # Disable windos upscale, when drop windpw on sreen adge
+  ["/org/gnome/mutter/edge-tiling"]="false"                                   # Disable windos upscale, when drop window on sreen adge
   ["/org/gnome/shell/keybindings/show-screenshot-ui"]="['<Shift><Super>4']"   # Capture a portion of the screen
   ["/org/gnome/shell/keybindings/screenshot"]="['<Shift><Super>3']"           # Capture the entire screen
   ["/org/gnome/desktop/wm/keybindings/close"]="['<Super>q']"                  # Close window
   ["/org/gnome/desktop/wm/keybindings/minimize"]="['<Super>m']"               # Hide window
-  ["/org/gnome/shell/keybindings/toggle-overview"]="['<Super>space']"         # Toogle application overview & search
+  ["/org/gnome/shell/keybindings/toggle-overview"]="['<Super>space']"         # Togle application overview & search
   ["/org/gnome/mutter/overlay-key"]="''"                                      # Disable search application by [Super]
   ["/org/gnome/settings-daemon/plugins/media-keys/screensaver"]="['<Control><Super>q']"   # Lock screen
 
@@ -27,8 +27,6 @@ declare -A keymap=(
   ["/org/gnome/desktop/wm/keybindings/switch-windows-backward"]="['']"
 
   # Workspace switching
-  ["/org/gnome/desktop/wm/keybindings/switch-to-workspace-right"]="['']"
-  ["/org/gnome/desktop/wm/keybindings/switch-to-workspace-left"]="['']"
   ["/org/gnome/desktop/wm/keybindings/switch-to-workspace-right"]="['']"
   ["/org/gnome/desktop/wm/keybindings/switch-to-workspace-left"]="['']"
   ["/org/gnome/desktop/wm/keybindings/switch-to-workspace-up"]="['']"
